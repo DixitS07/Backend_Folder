124ae9cc40e3b0bd90f00382bdb273dd36b0e008
surendra44 <71259552+surendra44@users.noreply.github.com>
2022-10-31 14:26:53 +0530

    as

--- model/user.js
+++ model/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 const userSchema = new Schema({
+     "photo":String,
      "firstName":String,
      "lastName":String,
      "age":Number,
